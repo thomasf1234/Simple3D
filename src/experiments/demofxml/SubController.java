bed9f84707bfc95bfeddbf62703309529364da74
Home <you@example.com>
2017-07-31 18:58:32 +0100

    [#0] TF : Adding demofxml package with vision to migrate gui structure

---
+++ src/experiments/demofxml/SubController.java
@@ -0,0 +1,17 @@
+package experiments.demofxml;
+
+import javafx.fxml.FXML;
+import javafx.scene.Group;
+import javafx.scene.control.Label;
+
+public class SubController {
+
+    @FXML public Label myLabel;
+    @FXML public Group root;
+    @FXML public Group editGroup;
+    @FXML public Group nonEditGroup;
+
+    public void initialize() {
+        myLabel.setText("blobby");
+    }
+}
